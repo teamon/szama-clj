42c2895821501d5992a9f49eeec205a60d9da5a3
Tymon Tobolski <i@teamon.eu>
2013-09-09 23:45:32 +0200

    Heroku part 3

--- project.clj
+++ project.clj
@@ -10,6 +10,7 @@
                   [ring/ring-jetty-adapter "1.1.6"]]
   :plugins [[lein-ring "0.8.5"]
             [drift "1.5.2"]]
+  :min-lein-version "2.0.0"
   :ring {:handler szama.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.5"]]}})
